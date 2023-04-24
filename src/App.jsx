cce541433edb73e68af51dd4c68e72a946635ce7
Erik Opata <15911873+ErikHPO@users.noreply.github.com>
2023-04-24 11:56:51 -0300

    more theme properties added

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,21 @@ import Navbar from './components/Navbar/Index'
 import Checkout from './view/Checkout'
 import Footer from './components/Footer/Index'
 import { createTheme } from '@mui/material/styles';
-import { ThemeProvider } from '@emotion/react'
-import { Container } from '@mui/material'
+import { ThemeProvider, useTheme } from '@emotion/react'
+import { Container, useMediaQuery } from '@mui/material'
 
 const theme = createTheme({
+  overrides: {
+    MuiContainer: {
+      root: {
+        "&.checkout": {
+          display: 'flex',
+          backgroundColor: 'red',
+          padding: '0',
+        }
+      }
+    }
+  },
   status: {
     danger: '#e53e3e',
   },
@@ -15,26 +26,42 @@ const theme = createTheme({
     primary: {
       darker: '#192C52',
       main: '#1E3460',
+      light: '#1E3460'
+    },
+    secondary: {
+      main: '#048189',
+      light: '#04818960'
     },
     neutral: {
       main: '#64748B',
       contrastText: '#fff',
+      grey: '#E8E8E8',
     },
     checkout:{
       main: '#01BB00'
     }
   },
+  stepper:{
+    root: {
+      width: "100%",
+    },
+    mobileStepper: {
+      position: "fixed",
+      bottom: 0,
+      width: "100%",
+      backgroundColor: 'red',
+    },
+  }
 });
 
 
 function App() {
-
   return (
     <div className="App">
       <ThemeProvider theme={theme}>
 
       <Navbar/>
-      <Container maxWidth='lg' className="checkout" >
+      <Container className="checkout" id="checkout-container" >
         <Checkout/>
       </Container>
       <Footer/>
